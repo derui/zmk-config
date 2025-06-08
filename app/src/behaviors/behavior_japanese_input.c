019751cacbfb99d7bd6f0e8fcf095d722f7f8bcf
derui <derutakayu@gmail.com>
2025-06-08 10:47:41 +0900

    feat: add initial behavior

---
+++ app/src/behaviors/behavior_japanese_input.c
@@ -0,0 +1,274 @@
+/*
+ * Copyright (c) 2025 The ZMK Contributors
+ *
+ * SPDX-License-Identifier: MIT
+ */
+
+#define DT_DRV_COMPAT zmk_behavior_japanese_input
+#define JAPANESE_INPUT_CAPTURE_SIZE 3
+
+#include <zephyr/device.h>
+#include <drivers/behavior.h>
+#include <zephyr/logging/log.h>
+#include <dt-bindings/zmk/keys.h>
+
+#include <zmk/event_manager.h>
+#include <zmk/events/keycode_state_changed.h>
+#include <zmk/behavior.h>
+
+LOG_MODULE_DECLARE(zmk, CONFIG_ZMK_LOG_LEVEL);
+
+// data model of the japanese input
+struct behavior_japanese_input_definition {
+  // resultと対応するキーコードのマッピング。releaseされたときに適用される
+  uint32_t mapping[];
+
+  // 実際に押されるキーコードの一覧。0から順に送出される
+  uint32_t result_seq[];
+};
+
+
+/*   /\* 各シフトシーケンスを生成するためのdefinition */
+/*    次のように使う。 */
+
+/*    NSI(A) => Aの単打 */
+/*    NM2(F, K) => FとKの同時シフト */
+/*    MN3(F, K, Q) => AとKとQの同時シフト */
+/*  *\/ */
+/* #define NSI(k, seq) { N_ ## k, seq } */
+/* #define NSS(k, seq) { SHIFT_BIT | N_ ## k, seq } */
+/* #define NM2(k1, k2, seq) { (N_ ## k1 | (N_ ## k2 << 5)), seq } */
+/* #define NM3(k1, k2, k3, seq) { N_ ## k1 | (N_ ## k2 << 5) | (N_ ## k3 << 10), seq} */
+
+/* /\* シフトキーを定義する構造体。 *\/ */
+/* typedef struct { */
+/*   /\* LSB is shift mark. *\/ */
+/*   uint16_t keycodes; */
+/*   const char* sequence; */
+/* } seq_definition_t; */
+
+/* /\* 複数キーの定義 *\/ */
+/* seq_definition_t seq_definitions[] = { */
+  
+/*   /\* Q行 *\/ */
+/*   NSI(W, "ku"), */
+/*   NSI(E, "si"), */
+/*   NSI(R, "ma"), */
+/*   NSI(U, "wo"), */
+/*   NSI(I, "ki"), */
+/*   NSI(O, "su"), */
+
+/*   /\* A行 *\/ */
+/*   NSI(A, "ta"), */
+/*   NSI(S, "u"), */
+/*   NSI(D, "na"), */
+/*   NSI(F, "ka"), */
+/*   NSI(G, "te"), */
+/*   NSI(H, "ni"), */
+/*   NSI(J, "ru"), */
+/*   NSI(K, "no"), */
+/*   NSI(L, "i"), */
+/*   NSI(SCLN, "nn"), */
+  
+/*   /\* Z行 *\/ */
+/*   NSI(Z, "ko"), */
+/*   NSI(X, "ha"), */
+/*   NSI(C, "mo"), */
+/*   NSI(V, "to"), */
+/*   NSI(B, "a"), */
+/*   NSI(N, "o"), */
+/*   NSI(M, "ra"), */
+/*   NSI(COMM, "ri"), */
+/*   NSI(DOT, "xtu"), */
+/*   NSI(SLSH, "tu"), */
+
+/*   /\* シフト面の定義 *\/ */
+/*   /\* Q行 *\/ */
+/*   NM2(K, E, "re"), */
+/*   NM2(K, R, "ti"), */
+/*   NM2(D, U, "so"), */
+/*   NM2(D, I, "ne"), */
+/*   NM2(D, O, "nu"), */
+
+/*   /\* A行 *\/ */
+/*   NM2(K, A, "me"), */
+/*   NM2(K, S, "se"), */
+/*   NM2(K, D, SS_TAP(X_MINS)), */
+/*   NM2(K, F, "mu"), */
+/*   NM2(K, G, "mi"), */
+/*   NM2(D, H, "yu"), */
+/*   NM2(D, J, "wa"), */
+/*   /\* NM2(D, K, "mu"), *\/ */
+/*   NM2(D, L, "he"), */
+/*   NM2(D, SCLN, "ho"), */
+  
+/*   /\* Z行 *\/ */
+/*   NM2(K, Z, "e"), */
+/*   NM2(K, X, "ro"), */
+/*   NM2(K, C, "ke"), */
+/*   /\* NM2(K, V, SS_TAP(X_MINS)), *\/ */
+/*   NM2(K, B, "sa"), */
+/*   NM2(D, N, "hi"), */
+/*   NM2(D, M, "yo"), */
+/*   /\* NM2(D, COMM, "hu"), *\/ */
+/*   NM2(D, DOT, "hu"), */
+/*   NM2(D, SLSH, "ya"), */
+
+/*   /\* 濁音 *\/ */
+/*   /\* あ行 *\/ */
+/*   NSI(Q, "vu"), */
+  
+/*   /\* か行 *\/ */
+/*   NM2(F, J, "ga"), */
+/*   NM2(F, I, "gi"), */
+/*   NM2(J, W, "gu"), */
+/*   NM2(J, C, "ge"), */
+/*   NM2(J, Z, "go"), */
+
+/*   /\* さ行 *\/ */
+/*   NM2(J, B, "za"), */
+/*   NM2(J, E, "zi"), */
+/*   NM2(F, O, "zu"), */
+/*   NM2(J, S, "ze"), */
+/*   NM2(F, U, "zo"), */
+
+/*   /\* た行 *\/ */
+/*   NM2(J, A, "da"), */
+/*   NM2(J, R, "di"), */
+/*   NM2(F, SLSH, "du"), */
+/*   NM2(J, G, "de"), */
+/*   NM2(J, V, "do"), */
+
+/*   /\* は行 *\/ */
+/*   NM2(J, X, "ba"), */
+/*   NM2(F, N, "bi"), */
+/*   NM2(F, DOT, "bu"), */
+/*   NM2(F, L, "be"), */
+/*   NM2(F, SCLN, "bo"), */
+
+/*   /\* 半濁音 *\/ */
+/*   /\* は行 *\/ */
+/*   NM2(M, X, "pa"), */
+/*   NM2(V, N, "pi"), */
+/*   NM2(V, DOT, "pu"), */
+/*   NM2(V, L, "pe"), */
+/*   NM2(V, SCLN, "po"), */
+  
+/*   /\* 小書き *\/ */
+/*   NM2(P, B, "xa"), */
+/*   NM2(Q, L, "xi"), */
+/*   NM2(P, S, "xu"), */
+/*   NM2(P, Z, "xe"), */
+/*   NM2(T, N, "xo"), */
+/*   NM2(Q, M, "xyo"), */
+/*   NM2(T, H, "xyu"), */
+/*   NM2(Q, SLSH, "xya"), */
+  
+/*   /\* 特殊 *\/ */
+/*   NM2(K, L, SS_TAP(X_DOT)), */
+/*   NM2(D, S, SS_TAP(X_COMM)), */
+/*   NM3(J, K, E, SS_TAP(X_SLSH)), */
+/*   NM3(J, K, D, "?"), */
+/*   NM3(J, K, C, "!"), */
+
+/*   /\* 拗音拡張 *\/ */
+/*   /\* 濁音 *\/ */
+/*   /\* か行 *\/ */
+/*   NM2(Q, I, "kya"), */
+/*   NM2(Z, I, "kyu"), */
+/*   NM2(A, I, "kyo"), */
+/*   NM3(F, Q, I, "gya"), */
+/*   NM3(F, Z, I, "gyu"), */
+/*   NM3(F, A, I, "gyo"), */
+
+/*   /\* さ行 *\/ */
+/*   NM2(P, E, "sya"), */
+/*   NM2(SLSH, E, "syu"), */
+/*   NM2(SCLN, E, "syo"), */
+/*   NM3(J, P, E, "zya"), */
+/*   NM3(J, SLSH, E, "zyu"), */
+/*   NM3(J, SCLN, E, "zyo"), */
+
+/*   /\* た行 *\/ */
+/*   NM2(P, R, "tya"), */
+/*   NM2(SLSH, R, "tyu"), */
+/*   NM2(SCLN , R, "tyo"), */
+/*   NM3(J, P, R, "dya"), */
+/*   NM3(J, SLSH, R, "dyu"), */
+/*   NM3(J, SCLN, R, "dyo"), */
+
+/*   /\* は行 *\/ */
+/*   NM2(Q, N, "hya"), */
+/*   NM2(Z, N, "hyu"), */
+/*   NM2(A, N, "hyo"), */
+/*   NM3(F, Q, N, "bya"), */
+/*   NM3(F, Z, N, "byu"), */
+/*   NM3(F, A, N, "byo"), */
+/*   NM3(V, Q, N, "pya"), */
+/*   NM3(V, Z, N, "pyu"), */
+/*   NM3(V, A, N, "pyo"), */
+
+/*   /\* な行 *\/ */
+/*   NM2(Q, H, "nya"), */
+/*   NM2(Z, H, "nyu"), */
+/*   NM2(A, H, "nyo"), */
+
+/*   /\* ま行 *\/ */
+/*   NM2(P, G, "mya"), */
+/*   NM2(SLSH, G, "myu"), */
+/*   NM2(SCLN, G, "myo"), */
+  
+/*   /\* ら行 *\/ */
+/*   NM2(Q, COMM, "rya"), */
+/*   NM2(Z, COMM, "ryu"), */
+/*   NM2(A, COMM, "ryo"), */
+/* }; */
+struct behavior_japanese_input_definition behavior_japanese_input_definitions[] = {
+
+};
+
+// 押されているkeycodeを保持する配列
+uint32_t captured_keycodes[JAPANESE_INPUT_CAPTURE_SIZE] = {0, 0, 0};
+
+// 対象とするキーコードをcaptureする
+// captureされたkeycodeは常に0番目に設定され、各々の値は後ろにずらしていく
+void japanese_input_capture_keycode(uint32_t keycode) {
+    for (int i = 0; i < JAPANESE_INPUT_CAPTURE_SIZE - 1; i++) {
+      captured_keycodes[i + 1] = captured_keycodes[i];
+    }
+    captured_keycodes[0] = keycode;
+}
+
+static int on_japanese_input_binding_pressed(struct zmk_behavior_binding *binding,
+                                     struct zmk_behavior_binding_event event) {
+    LOG_DBG("position %d keycode 0x%02X", event.position, binding->param1);
+    return raise_zmk_keycode_state_changed_from_encoded(binding->param1, true, event.timestamp);
+}
+
+static int on_japanese_input_binding_released(struct zmk_behavior_binding *binding,
+                                      struct zmk_behavior_binding_event event) {
+    LOG_DBG("position %d keycode 0x%02X", event.position, binding->param1);
+    return raise_zmk_keycode_state_changed_from_encoded(binding->param1, false, event.timestamp);
+}
+
+static const struct behavior_driver_api behavior_japanese_input_driver_api = {
+    .binding_pressed = on_japanese_input_binding_pressed,
+    .binding_released = on_japanese_input_binding_released,
+};
+
+static int japanese_input_init(const struct device *dev) {
+  static bool initialized = false;
+
+  if (!initialized) {
+    // initialize the captured keycodes to zero
+    captured_keycodes = {0,0,0};
+  }
+  initialized = true;
+  return 0;
+};
+
+#define KP_INST(n)                                                                                 \
+    BEHAVIOR_DT_INST_DEFINE(n, japanese_input_init, NULL, NULL, NULL, POST_KERNEL,                                \
+                            CONFIG_KERNEL_INIT_PRIORITY_DEFAULT, &behavior_key_press_driver_api);
+
+DT_INST_FOREACH_STATUS_OKAY(KP_INST)
