e1df59200d6b2b2ced2f3e185048b1ec81b27055
derui <derutakayu@gmail.com>
2025-06-28 09:58:43 +0900

    fix: use valid GPIO for DR

--- build-local.sh
+++ build-local.sh
@@ -21,10 +21,17 @@ fi
 
 west zephyr-export
 
-west build -s zmk/app -p -d build -b nice_nano_v2 -S studio-rpc-usb-uart -- \
-     -DSHIELD=orbit_one \
+west build -s zmk/app -p -d build -b seeeduino_xiao_ble -S studio-rpc-usb-uart -- \
+     -DSHIELD=orbit_one_left \
      -DCONFIG_ZMK_STUDIO=y \
      -DZMK_EXTRA_MODULES='/workspaces/zmk-config' \
      -DZMK_CONFIG=$BASE_DIR/config
 
-mv build/zephyr/zmk.uf2 /workspaces/zmk-config/build-local.uf2
+mv build/zephyr/zmk.uf2 /workspaces/zmk-config/left.uf2
+
+west build -s zmk/app -p -d build -b seeeduino_xiao_ble -S studio-rpc-usb-uart -- \
+     -DSHIELD=orbit_one_right \
+     -DZMK_EXTRA_MODULES='/workspaces/zmk-config' \
+     -DZMK_CONFIG=$BASE_DIR/config
+
+mv build/zephyr/zmk.uf2 /workspaces/zmk-config/left.uf2
