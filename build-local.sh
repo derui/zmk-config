1bedffe7f0262e2ea9198cc890a8f600f5fb94b6
derui <derutakayu@gmail.com>
2025-08-14 10:21:53 +0900

    fix: spi and i2c configurations

--- build-local.sh
+++ build-local.sh
@@ -22,21 +22,20 @@ fi
 west zephyr-export
 
 west build -s zmk/app -p -d build -b seeeduino_xiao_ble -S studio-rpc-usb-uart -- \
-     -DSHIELD=orbit_one_left \
+     -DSHIELD="planetes42_left" \
      -DCONFIG_ZMK_STUDIO=y \
      -DZMK_EXTRA_MODULES='/workspaces/zmk-config' \
      -DZMK_CONFIG=$BASE_DIR/config
 
 # west build -s zmk/app -p -d build -b seeeduino_xiao_ble -S zmk-usb-logging -- \
-#      -DSHIELD=orbit_one_left \
+#      -DSHIELD=planetes42_left \
 #      -DZMK_EXTRA_MODULES='/workspaces/zmk-config' \
 #      -DZMK_CONFIG=$BASE_DIR/config
-
 
 mv build/zephyr/zmk.uf2 /workspaces/zmk-config/left.uf2
 
 west build -s zmk/app -p -d build -b seeeduino_xiao_ble -- \
-     -DSHIELD=orbit_one_right \
+     -DSHIELD="planetes42_right nice_view" \
      -DZMK_EXTRA_MODULES='/workspaces/zmk-config' \
      -DZMK_CONFIG=$BASE_DIR/config
 
