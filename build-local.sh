23ef57fd5878ad6cbd50bda20c32b8210f841766
derui <derutakayu@gmail.com>
2025-08-23 17:28:14 +0900

    fix: some configurations for planetes42

--- build-local.sh
+++ build-local.sh
@@ -21,16 +21,18 @@ fi
 
 west zephyr-export
 
-west build -s zmk/app -p -d build -b seeeduino_xiao_ble -S studio-rpc-usb-uart -- \
-     -DSHIELD="planetes42_left" \
-     -DCONFIG_ZMK_STUDIO=y \
-     -DZMK_EXTRA_MODULES='/workspaces/zmk-config' \
-     -DZMK_CONFIG=$BASE_DIR/config
-
-# west build -s zmk/app -p -d build -b seeeduino_xiao_ble -S zmk-usb-logging -- \
-#      -DSHIELD=planetes42_left \
+# west build -s zmk/app -p -d build -b seeeduino_xiao_ble -S studio-rpc-usb-uart -- \
+#      -DSHIELD="planetes42_left" \
+#      -DCONFIG_ZMK_STUDIO=y \
 #      -DZMK_EXTRA_MODULES='/workspaces/zmk-config' \
 #      -DZMK_CONFIG=$BASE_DIR/config
+
+west build -s zmk/app -p -d build -b seeeduino_xiao_ble \
+     -S zmk-usb-logging \
+     -- \
+     -DSHIELD="planetes42_left" \
+     -DZMK_EXTRA_MODULES='/workspaces/zmk-config' \
+     -DZMK_CONFIG=$BASE_DIR/config
 
 mv build/zephyr/zmk.uf2 /workspaces/zmk-config/left.uf2
 
